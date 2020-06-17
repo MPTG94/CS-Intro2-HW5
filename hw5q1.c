fccf8d94ea34ff15a5ef0e4705a4744a407e4fc8
Mor Paz <1912.mor@gmail.com>
2020-06-17 16:46:44 +0300

    Finish Q1 write shell of Q2

--- hw5q1.c
+++ hw5q1.c
@@ -2,17 +2,59 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void PrintEnterNumber()
-{
+void PrintEnterNumber();
+
+void PrintIsPalindrome();
+
+void PrintIsNotPalindrome();
+
+int ReverseNum(int num, int temp);
+
+int main() {
+    int num = 0;
+    PrintEnterNumber();
+    scanf("%d", &num);
+    int temp = ReverseNum(num, 0);
+
+    if (num == temp) {
+        PrintIsPalindrome();
+    } else {
+        PrintIsNotPalindrome();
+    }
+    return 0;
+}
+
+/**
+ * Function to reverse the digits of a number,
+ * The function receives the number to reverse and the temporary reversal result
+ * The function will add the last digit of the input number to the end of the result
+ * and recursively call the next iteration with the input number without it's last digit
+ * @param num The number to reverse
+ * @param temp The temporary reversal result
+ * @return The reversed number
+ */
+int ReverseNum(int num, int temp) {
+    if (num == 0) {
+        // We finished reversing the number's digits
+        return temp;
+    }
+
+    // Add the last digit of num to the end of temp
+    temp = (temp * 10) + (num % 10);
+
+    // Call a recursive reversal with the number without it's last digit
+    // (Because we already took care of it)
+    return ReverseNum(num / 10, temp);
+}
+
+void PrintEnterNumber() {
     printf("Please enter a number:\n");
 }
 
-void PrintIsPalindrome()
-{
-	printf("It's a palindrome!");
+void PrintIsPalindrome() {
+    printf("It's a palindrome!");
 }
 
-void PrintIsNotPalindrome()
-{
-	printf("It's not a palindrome!");
+void PrintIsNotPalindrome() {
+    printf("It's not a palindrome!");
 }
--- hw5q2.c
+++ hw5q2.c
@@ -2,32 +2,65 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void PrintWelcomeMessage()
-{
+#define N 4
+
+void PrintWelcomeMessage();
+
+void PrintSourceCityMessage();
+
+void PrintDestinationCityMessage();
+
+void PrintShortestPathMessage();
+
+void PrintNumber(int num);
+
+void PrintWhiteSpace();
+
+int main() {
+    int roads[N][N] = {{0}};
+
+    PrintWelcomeMessage();
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            scanf("%d", &roads[i][j]);
+        }
+    }
+
+    int source = 0;
+    PrintSourceCityMessage();
+    scanf("%d", &source);
+
+    int dest = 0;
+
+    PrintDestinationCityMessage();
+    scanf("%d", &dest);
+
+    PrintShortestPathMessage();
+
+
+    return 0;
+}
+
+void PrintWelcomeMessage() {
     printf("Please enter road matrix:\n");
 }
 
-void PrintSourceCityMessage()
-{
+void PrintSourceCityMessage() {
     printf("Please enter source city:\n");
 }
 
-void PrintDestinationCityMessage()
-{
+void PrintDestinationCityMessage() {
     printf("Please enter destination city:\n");
 }
 
-void PrintShortestPathMessage()
-{
+void PrintShortestPathMessage() {
     printf("The shortest path is:\n");
 }
 
-void PrintNumber(int num)
-{
-	printf("%d", num);
+void PrintNumber(int num) {
+    printf("%d", num);
 }
 
-void PrintWhiteSpace()
-{
-	printf(" ");
+void PrintWhiteSpace() {
+    printf(" ");
 }
